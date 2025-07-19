dec4c99295d9ba76bfca9469788f9c0f8376904b
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2025-07-20 00:26:19 +0300

    update gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  eslintConfigPrettier
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  // "ignorePatterns": ["allure/**"]
 ]);
